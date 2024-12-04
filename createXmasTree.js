ff72e12927ea72653d4f7e45c0fadf49135eb2ca
CascoOnce <nick.carrascovillegas@hotmail.com>
2024-12-04 08:55:52 -0500

    AdventJS_4

---
+++ createXmasTree.js
@@ -0,0 +1,50 @@
+function createXmasTree(height, ornament) {
+    /* Code here */
+    let maxBase = (2*height) - 1;
+    let chain = '';
+    for(let i = 1; i <= height; i++){
+        let repeat = 2*i - 1, underscore = (maxBase - repeat)/2;
+        chain += '_'.repeat(underscore);
+        chain += ornament.repeat(repeat);
+        chain += '_'.repeat(underscore);
+        chain += '\n';
+    }
+    chain += '_'.repeat((maxBase-1)/2) + '#' + '_'.repeat((maxBase-1)/2) + '\n';
+    chain += '_'.repeat((maxBase-1)/2) + '#' + '_'.repeat((maxBase-1)/2);
+    return chain;
+}
+
+const tree = createXmasTree(5, '*')
+console.log(tree)
+/*
+____*____
+___***___
+__*****__
+_*******_
+*********
+____#____
+____#____
+*/
+
+const tree2 = createXmasTree(3, '+')
+console.log(tree2)
+/*
+__+__
+_+++_
++++++
+__#__
+__#__
+*/
+
+const tree3 = createXmasTree(6, '@')
+console.log(tree3)
+/*
+_____@_____
+____@@@____
+___@@@@@___
+__@@@@@@@__
+_@@@@@@@@@_
+@@@@@@@@@@@
+_____#_____
+_____#_____
+*/
